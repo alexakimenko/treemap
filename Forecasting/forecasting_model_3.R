cd5ac222c13489af98ff608e92f0f02f0377a897
finnfromfinland <alexandr.akimenko@gmail.com>
2016-06-15 21:07:09 +0300

    Add files via upload

--- Forecasting/forecasting_model_3.R
+++ Forecasting/forecasting_model_3.R
@@ -2,49 +2,38 @@
 #install.packages("reshape")
 #install.packages("xts")
 #install.packages("plyr")
+#install.packages("XLConnect")
 library(forecast)
 library(reshape)
 library(xts)
 library(plyr)
+library(XLConnect)
 
+
+####### PARAMETERS ###########
+
+# this needs to be changed in case of new user
+path<-"//vkovnazcti0023/COL/0-Collections_Strategy_and_System_Support/STRATEGY/Akimenko/pilots/forecasting/" # current working directory
+n.ahead<-6   # number of periods to forecast
+
+
+####### FUNCTIONS ###########
+
+
+# the core of the model - STL decomposition
 forecastStl <- function(myTs,var_name, n.ahead = 6) {
   fit.stl <- stl(myTs,t.window=8, s.window = 'periodic')
   sts <- fit.stl$time.series
   fore <- forecast(fit.stl, h = n.ahead, level = 95)
   fore.seasonal<-lag(sts[,1],-12)*(fore$mean/fore$mean)
   fore.trend<-fore$mean-fore.seasonal
-  plot(sts,main=paste0(var_name," - STL Decompose"))
+  #x<-plot(sts,main=paste0(var_name," - STL Decompose"))
   pdf(paste0(path,"output/",var_name,"_stl_decompose.pdf"))
-  plot(sts)
+  plot(sts,main=paste0(var_name," - STL Decompose"))
   dev.off( )
   return(cbind(forecast=fore$mean,
                trend=fore.trend,seasonal=fore.seasonal))
 }
-
-
-avg_l3_ns_months<-function(myTs,n.ahead = 6){
-  myTs_cleared<-subset(myTs,season=c(2,3,4,6,7,8,9,10,11))
-  pred<-rep(mean(tail(myTs_cleared,3)),n.ahead )
-  start<-tsp(myTs)[2]+2/12
-  start<-c(trunc(start),start%%1*12)     # incorrect for Dec!
-  pred<-ts(pred,start=start, freuency=12) 
-  return(pred)
-}
-
-
-
-
-#path<-"/Users/AlexAkimenko/Documents/Ñ€Ð°Ð±Ð¾Ñ‚Ð°/Citi/Forecasting/"
-path<-"//vkovnazcti0023/COL/0-Collections_Strategy_and_System_Support/STRATEGY/Akimenko/pilots/forecasting/"
-v<-read.csv(paste0(path,"input.csv"),header=T,sep=";")
-variables<-colnames(v)[2:ncol(v)]
-start_month<-c(2012,12)  # correct - needs to be selected from input data (min month+1 due to net flow logic)
-end_month<-c(2016, 4) # correct - needs to be selected from input data
-forecast_month<-forecast_month<-format(Sys.time(), "%Y-%m-01")
-n.ahead<-6
-v<-ts(v[,-1], start=start_month, end=end_month, frequency=12)
-
-
 
 
 # net flow model works on product level only. Thus it requires specific input- time series data ("v") with column names containing product name ("prod")
@@ -72,7 +61,7 @@ net_flow_model<-function(prod,v=v, n.ahead=6){
       output3<-cbind(bucket0_decom,y_decom)
       names<-c(paste0(colnames(ts)[i-1],"_",colnames(bucket0_decom)),name)
     } else {
-      bucket<-c(as.xts(ts[,i]),lag(output[,i-1],1)*as.xts(y_pred)) # Ð·Ð´ÐµÑÑŒ Ð±ÐµÑ€ÐµÑ‚ÑÑ Ð¸Ð· Ñ‚Ð°Ð±Ð»Ð¸Ñ†Ñ‹ Ñ actuals+forecast
+      bucket<-c(as.xts(ts[,i]),lag(output[,i-1],1)*as.xts(y_pred)) # çäåñü áåðåòñÿ èç òàáëèöû ñ actuals+forecast
       output<-cbind(output,bucket)
       output2<-cbind(output2,nf)
       output3<-cbind(output3,y_decom)
@@ -85,8 +74,7 @@ net_flow_model<-function(prod,v=v, n.ahead=6){
 }
 
 
-
-
+# calculates mean absolute persentage error. Main inputs are time series on product level and net_flow_model
 
 MAPE<-function(prod,v=v, n.ahead=6){
   test_df<-data.frame()
@@ -111,51 +99,20 @@ MAPE<-function(prod,v=v, n.ahead=6){
 }
 
 
+####### DATA LOAD ###########
+
+wb <- loadWorkbook(paste0(path,"template.xlsm"))
+v <- readWorksheet(wb, sheet = "input_data", header = TRUE)
+variables<-colnames(v)[2:ncol(v)]
+start_month<-as.integer(c(format(min(v$YearMonth),"%Y"),format(min(v$YearMonth),"%m")))
+end_month<-as.integer(c(format(max(v$YearMonth),"%Y"),format(max(v$YearMonth),"%m")))
+forecast_month<-forecast_month<-format(Sys.time(), "%Y-%m-01")
+v<-ts(v[,-1], start=start_month, end=end_month, frequency=12)
+dir.create(file.path(paste0(path, "/output")), showWarnings = FALSE)
 
 
-###  validation summary ####
-MAPE_CC<-MAPE("CC",v,n.ahead)
-MAPE_CC<-MAPE_CC[MAPE_CC$YearMonth>=seq(max(MAPE_CC$YearMonth), length=2, by="-2 years")[2],] # this to have foreast and actuals for last 2 years always
-t1<-MAPE_CC[grep("MAPE",MAPE_CC$variable),]
-t1<-aggregate(t1$value,by=list(t1$variable,t1$n_ahead),FUN=mean, na.rm=TRUE)
+####### FORECAST ###########
 
-MAPE_PIL<-MAPE("PIL",v,n.ahead)
-t2<-MAPE_PIL[grep("MAPE",MAPE_PIL$variable),]
-
-output2<-rbind(t1,t2)
-colnames(output2)<-c("variable","n_ahead","MAPE")
-output2<-output2[output2$n_ahead>0,]
-t3<-aggregate(output2$MAPE,by=list(output2$variable),FUN=mean, na.rm=TRUE)
-
-colnames(t3)<-c("variable","MAPE")
-t3$n_ahead<-"Cumulative"
-output2<-rbind(output2,t3)
-output2$forecast_month<-forecast_month
-write.table(output2, paste0(path,"/output/validation_summary.txt"), sep="\t",row.names=F)	
-
-# CC
-# last 3 months average model - 14.4%, 12.5%, 13.6%, 13.4%, 12.2%, 10.6% (12.8%)
-# bucket stl model - 4.7%, 6.5%, 6.8%, 10.5%, 12.9%, 10.8%, 9.4%  (8.8%)
-# accuracy increase is 31%
-# net flow stl model - 4.7%, 8.0%, 5.6%, 4.1%, 4.0%, 2.8%, 2.7% (4.6%)
-# accuracy increase is 48% (64% total!)
-
-# PIL
-# last 3 months average model - 6.3%, 8.6%, 9.5%, 11.0%, 11.7% (9.4%)
-# bucket stl model 4.8%, 13.3%, 8.4%, 11.4%, 15.0% (10.6%)
-# net flow stl model - 4.8%, 11.6%, 10.1%, 9.8%, 13.4% (9.9%)
-# accuracy increase is 6% (-5% total)
-
-
-###  validation results
-output<-rbind(MAPE_CC,MAPE_PIL)
-output_c<-cast(output, n_ahead + variable ~ YearMonth)
-output_c[is.na(output_c)] <- ""
-output_c<-output_c[!(grepl("Actual",output_c$variable) & output_c$n_ahead!=0),]
-output_c<-output_c[!(!grepl("Actual",output_c$variable) & output_c$n_ahead==0),]
-write.table(output_c, paste0(path,"/output/validation_results.txt"), sep="\t",row.names=F)	
-
-###  forecast
 pred_CC<-net_flow_model("CC",v,n.ahead)
 pred_PIL<-net_flow_model("PIL",v,n.ahead)
 
@@ -176,71 +133,42 @@ CC_PIL_decom_df<-cbind(variable=rownames(CC_PIL_decom_df),CC_PIL_decom_df)
 
 output3<-rbind.fill(CC_PIL_nf_df,CC_PIL_df,CC_PIL_decom_df)
 output3[is.na(output3)] <- ""
-write.table(output3, paste0(path,"/output/forecast.txt"), sep="\t",row.names=F)	
-
-#### deep dive into variables
+write.table(output3, paste0(path,"/output/forecast.txt"), sep="\t",row.names=F)
 
 
-for (i in 26:38){
-  end<-tsp(v)[1]+i/12
-  end<-c(trunc(end),end%%1*12)
-  ts<-window(v[,8],end=end)
-  fit.stl <- stl(ts,t.window=100, s.window = 'periodic')
-  fore <- forecast(fit.stl, h = n.ahead, level = 95)
-  if (i==26){
-    plot(v[,8])
-    lines(fore$mean,col="red")
-  } else {
-    lines(fore$mean,col="red")
-  }
-}
-for (i in 26:38){
-  end<-tsp(v)[1]+i/12
-  end<-c(trunc(end),end%%1*12)
-  ts<-window(v,end=end)
-  ts_pred<-net_flow_model(prod,ts,n.ahead)
-  if (i==26){
-    plot(as.xts(v[,9]))
-    lines(ts_pred[,2],col="red")
-  } else {
-    lines(ts_pred[,2],col="red")
-  }
-}
+####### VALIDATION SUMMARY ###########
 
-forecastStl <- function(myTs,var_name, n.ahead = 6) {
-  fit.stl <- stl(myTs,t.window=8, s.window = 'periodic')
-  sts <- fit.stl$time.series
-  fore <- forecast(fit.stl, h = n.ahead, level = 95)
-  #plot(sts,main=paste0(var_name," - STL Decompose"))
-  #pdf(paste0(path,"output/",var_name,"_stl_decompose.pdf"))
-  #plot(sts)
-  #dev.off( )
-  return(fore$mean)
-}
+MAPE_CC<-MAPE("CC",v,n.ahead)
+MAPE_CC<-MAPE_CC[MAPE_CC$YearMonth>=seq(max(MAPE_CC$YearMonth), length=2, by="-2 years")[2],] # this to have foreast and actuals for last 2 years always
+t1<-MAPE_CC[grep("MAPE",MAPE_CC$variable),]
+t1<-aggregate(t1$value,by=list(t1$variable,t1$n_ahead),FUN=mean, na.rm=TRUE)
+
+MAPE_PIL<-MAPE("PIL",v,n.ahead)
+t2<-MAPE_PIL[grep("MAPE",MAPE_PIL$variable),]
+t2<-aggregate(t2$value,by=list(t2$variable,t2$n_ahead),FUN=mean, na.rm=TRUE)
 
 
+output2<-rbind(t1,t2)
+colnames(output2)<-c("variable","n_ahead","MAPE")
+output2<-output2[output2$n_ahead>0,]
+t3<-aggregate(output2$MAPE,by=list(output2$variable),FUN=mean, na.rm=TRUE)
 
-#### deep dive into variables (avg_l3_ns_months)
-
-for (i in 26:38){
-  end<-tsp(v)[1]+i/12
-  end<-c(trunc(end),end%%1*12)
-  ts<-window(v[,8],end=end)
-  fore <- avg_l3_ns_months(ts,6)
-  if (i==26){
-    plot(v[,8])
-    lines(fore,col="red")
-  } else {
-    lines(fore,col="red")
-  }
-}
+colnames(t3)<-c("variable","MAPE")
+t3$n_ahead<-"Cumulative"
+output2<-rbind(output2,t3)
+output2$forecast_month<-forecast_month
+write.table(output2, paste0(path,"/output/validation_summary.txt"), sep="\t",row.names=F)	
 
 
-#install.packages("stsm")
-#install.packages("stsm.class")
-library(stsm.class)
-library(stsm)
-t<-window(v[,1],start=c(2013,1),end=c(2015,12))
-m <- stsm.model(model = "BSM", y = t, transPars = "StructTS")
-fit2 <- stsmFit(m, stsm.method = "maxlik.td.optim", method = "L-BFGS-B", 
-                KF.args = list(P0cov = TRUE))
+####### VALIDATION RESULTS ###########
+
+output<-rbind(MAPE_CC,MAPE_PIL)
+output_c<-cast(output, n_ahead + variable ~ YearMonth)
+output_c[is.na(output_c)] <- ""
+output_c<-output_c[!(grepl("Actual",output_c$variable) & output_c$n_ahead!=0),]
+output_c<-output_c[!(!grepl("Actual",output_c$variable) & output_c$n_ahead==0),]
+output_c<-rbind.fill(output3,output_c)
+output_c<-output_c[-c(1:nrow(output3)),]
+write.table(output_c, paste0(path,"/output/validation_results.txt"), sep="\t",row.names=F)	
+
+
